24e45536d664429ca22c1b9b6c6e124f299a6a83
OmarZiyada1 <omarziyada16@gmail.com>
2024-05-07 12:41:13 +0200

    reservation add to sync-db

--- sync-db.js
+++ sync-db.js
@@ -3,6 +3,7 @@ const Patient = require('./models/Patient');
 const Service = require('./models/Service');
 const Appointment = require('./models/Appointment');
 const Employee = require('./models/Employee');
+const Rservation = require('./models/Reservation');
 (async() => {
     await sequelize.sync({ force: true });
     console.log('All models were synchronized successfully.');
