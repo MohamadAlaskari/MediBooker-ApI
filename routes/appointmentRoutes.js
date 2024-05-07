bf6bfccae1501c8c531a95f5da670ab8d12c1ac8
MohamadAlaskari <m_alskari96@hotmail.com>
2024-05-07 13:09:44 +0200

    gd

--- routes/appointmentRoutes.js
+++ routes/appointmentRoutes.js
@@ -4,12 +4,12 @@ const appointmentController = require('../controllers/appointmentController');
 
 router.get('/appointments', appointmentController.getAllAppointments);
 
-router.get('/appointments/:id', appointmentController.getAppointmentById);
+router.get('/appointment', appointmentController.getAppointmentById);
 
-router.post('/addappointment', appointmentController.createAppointment);
+router.post('/create', appointmentController.createAppointment);
 
-router.put('/updateappointment/:id', appointmentController.updateAppointment);
+router.put('/update', appointmentController.updateAppointment);
 
-router.delete('/deleteappointment/:id', appointmentController.deleteAppointment);
+router.delete('/delete', appointmentController.deleteAppointment);
 
 module.exports = router;
